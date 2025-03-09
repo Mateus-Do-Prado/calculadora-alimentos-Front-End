0c7927ce81810c0fc23041c56d2e336938750b77
MATEUS DO PRADO ALCÂNTARA <mateprado123@gmail.com>
2025-03-09 08:03:27 -0400

    Inserindo o cabeçalho real do projeto com um menu lateral.

--- src/pages/header/MenuLateral.tsx
+++ src/pages/header/MenuLateral.tsx
@@ -14,8 +14,10 @@ function MenuLateral() {
     };
 
     return (
-        <div>
-            <div className={`menu ${isOpen ? 'open' : ''}`}>
+        
+<div className="cabecalho">
+    
+<div className={`menu ${isOpen ? 'open' : ''}`}>
                 <a href="#" className="closebtn" onClick={closeMenu}>&times;</a>
                 <a><Link to={"/"}>Início</Link></a>
                 <a><Link to={"/lista-cliente"}>Veja nossos clientes</Link></a>
@@ -24,11 +26,26 @@ function MenuLateral() {
             </div>
 
             <div className="main">
-                <span className="openbtn" onClick={openMenu}>&#9776; Abrir Menu</span>
+                <span className="openbtn" onClick={openMenu}>&#9776; </span>
     
             </div>
-        </div>
+
+
+  <div className="logo">
+    <div className="calculadora"><span className="calculadora_span">Calculadora</span></div>
+    <div className="de-aliment-s"><span className="dealiments_span">DE ALIMENTOS</span></div>
+  </div>
+  <div data-size="Small" data-state="Hover" data-variant="Primary" className="button">
+    <div className="button_01"><span className="button_01_span">Sua Conta</span></div>
+  </div>
+  <div data-size="Small" data-state="Hover" data-variant="Primary" className="button_02">
+    <div className="button_03"><span className="button_03_span">Sair</span></div>
+  </div>
+</div>
+        
+ 
     );
 };
 
 export default MenuLateral;
+
